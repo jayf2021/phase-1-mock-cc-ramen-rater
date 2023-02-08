965c592562b433e4a90173daacc06899d5e69693
James Fields <fieldsjay13@gmail.com>
2023-02-08 11:22:43 -0700

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 // write your code here
+const baseURL = "http://localhost:3000/ramens"
+const ramenMenuDiv = document.getElementById("ramen-menu");
+const ramenImage = document.createElement("img");
+
+fetch(baseURL)
+    .then((res) => res.json())
+    .then(ramens => renderRamens(ramens));
+
+function renderRamens(ramens) {
+    console.log(ramens)
+    ramens.forEach(renderRamen)
+}
+
+function renderRamen(ramen) {
+    ramenImage.src = ramen.image;
+    ramenMenuDiv.append(ramenImage);
+}
+
